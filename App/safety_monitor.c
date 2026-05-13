fe6e98db7e206793bd3b5c930ff71096312f567b
A.Ramy <ahmedramysaidelnaggar@gmail.com>
2026-05-14 00:43:48 +0300

    Fixed wrong led color

--- App/safety_monitor.c
+++ App/safety_monitor.c
@@ -22,7 +22,7 @@ void safetyTask(void *pvParameters)
         forceGateState(REVERSING); // FORCE sate to be reversing; so any input gets ignored
         
 				xQueueReset(ledQueue); // Flush the queue
-				const Led_t ev_led = EV_SET_RED;
+				const Led_t ev_led = EV_SET_GREEN;
 				xQueueSend(ledQueue, (void *)(&ev_led), 0); // Send ev_led to LED Task, then block.
 			
 				#if TEST_SAFETY
